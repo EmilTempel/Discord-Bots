7c28478fc211b4d30047424789f1c93a3425ad58
L0rdBob <84745032+L0rdBob@users.noreply.github.com>
2021-09-11 00:19:27 +0200

    shop+

--- Zitate-Bot2/src/potatocoin/Shop.java
+++ Zitate-Bot2/src/potatocoin/Shop.java
@@ -1,8 +1,8 @@
 package potatocoin;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
+import discord.UserInformation;
 import discord.Zitat;
 import net.dv8tion.jda.api.entities.Emote;
 import net.dv8tion.jda.api.entities.Guild;
@@ -10,13 +10,18 @@ import net.dv8tion.jda.api.entities.Guild;
 public class Shop {
 
 	double factor;
+	final static double variation = 0.1;
+	
 	ArrayList<Zitat> dailyOffer;
-	final static Dropable.Rarity[] excludedFromDailyOffers = {};
-	HashMap<LootBox, Double> lootBoxes;
+	final static int nrOffers = 1;
+	final static Dropable.Rarity[] excludedFromDailyOffers = {Dropable.Rarity.gaulitastisch, Dropable.Rarity.toxymoxy, Dropable.Rarity.marximal, Dropable.Rarity.wild};
+	
 	static Emote reactionBuy = null, reactionScrollUp = null, reactionScrollDown = null, reactionScrollRight = null, reactionScrollLeft = null;
 	final static String buyName = ":geldluis:", scrollUpId = "U+2B06", scrollDownId = "U+2B07", scrollRightId = "U+27A1", scrollLeftId = "U+2B05";
 	
-	public Shop(Guild g, double factor, ArrayList<Zitat> dailyOffer) {
+	UserInformation ui;
+	
+	public Shop(Guild g, UserInformation ui, double factor, ArrayList<Zitat> dailyOffer) {
 		reactionBuy = g.getEmotesByName(buyName, true).get(0);
 		reactionScrollUp = g.getJDA().getEmoteById(scrollUpId);
 		reactionScrollDown = g.getJDA().getEmoteById(scrollDownId);
@@ -26,5 +31,17 @@ public class Shop {
 		this.factor = factor;
 		this.dailyOffer = dailyOffer;
 	}
-
+	
+	public Zitat[] getIncludedAvailableZitate(){
+		return;
+	}
+	
+	public void refresh() {
+		factor += (((Math.random() * 2) - 1) * variation);
+		dailyOffer.clear();
+		for (int i = 0; i < nrOffers; i++) {
+			
+		}
+	}
+	
 }
